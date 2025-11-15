e1d092837ef38f19c5aef6b2480ae51d19190468
Ishan Singhal <ishansinghal123@gmail.com>
2025-11-15 08:20:32 -0600

    calendarFixed

--- src/config/googleScript.ts
+++ src/config/googleScript.ts
@@ -2,4 +2,4 @@
 // This URL is hardcoded and can only be changed by modifying the code
 
 export const GOOGLE_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbylHYOlG6DUeyILBVz_RsMhQax07N-dofD8ZqMiMZHkL3egKQXZ1s-jzAmRrkVEsAGSgA/exec";
+  "https://script.google.com/macros/s/AKfycbyPbo24PjUsJx60TkI967nar6z7hlg9wrzsrYzZ2eREmQzIzTwgSVVGwWmX3eMcEGqwOA/exec";
